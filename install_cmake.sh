1d95b33880adbcf48b0e91a53e0b6753c4a2a853
Esteban Hernandez <awseste@amazon.com>
2021-05-11 11:35:32 -0500

    changes to include new cmake path

--- install_cmake.sh
+++ install_cmake.sh
@@ -3,6 +3,7 @@
 yum install gcc-c++
 wget --quiet --no-check-certificate https://github.com/Kitware/CMake/releases/download/v3.20.2/cmake-3.20.2.tar.gz
 tar -zxvf cmake-*
+cd cmake*
 ./bootstrap
 make 
 sudo make install 
