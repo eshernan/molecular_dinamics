a52f078214b5c91f16df152c436909d5e80ed616
Esteban Hernandez <awseste@amazon.com>
2021-05-13 13:33:47 -0500

    including env to limited cuda architectures

--- gromacs/compile_gromacs_gpu.sh
+++ gromacs/compile_gromacs_gpu.sh
@@ -14,9 +14,13 @@ fi
 source ../config_intel.sh
 export BASE_REPO=$PWD
 cd $GROMACS_INSTALLER
+if [ ! -f $GROMACS_VERSION".tar.gz" ]; then 
+    source $BASE_REPO/download.sh 
+fi
+if [ ! -d $GROMACS_VERSION ]; then 
+    tar -zxvf $GROMACS_VERSION".tar.gz"
+fi
 
-source $BASE_REPO/download.sh 
-tar -zxvf gromacs-2021.2.tar.gz
 cd gromacs-*
 if [ -d build ]; then 
     rm -Rf  build
@@ -24,7 +28,7 @@ fi
 mkdir  build
 cd build
 
-FLAGS="-xCORE-AVX2 -g -static-intel"; CFLAGS=$FLAGS; CXXFLAGS=$FLAGS; CC=mpiicc; CXX=mpiicpc  cmake .. -DCMAKE_CXX_COMPILER=mpiicpc -DOpenMP_CXX_FLAGS="-qopenmp" -DOpenMP_CXX_LIB_NAMES="libiomp5" -DOpenMP_libiomp5_LIBRARY=/opt/intel/oneapi/compiler/latest/linux/compiler/lib/intel64_lin/libiomp5.so -DCMAKE_INSTALL_PREFIX=$GROMACS_BINARY -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON -DGMX_MPI=on -DGMX_GPU=CUDA -DGMX_CUDA_TARGET_SM="30;35;52;60;61;70;75"  -DGMX_OPENMP=ON -DGMX_SIMD=AVX2_256 -DGMX_CYCLE_SUBCOUNTERS=ON -DGMX_OPENMP_MAX_THREADS=32 -DBUILD_SHARED_LIBS=OFF  -DGMX_BUILD_HELP=OFF 
+FLAGS="-xCORE-AVX2 -g -static-intel"; CFLAGS=$FLAGS; CXXFLAGS=$FLAGS; CC=mpiicc; CXX=mpiicpc  cmake .. -DCMAKE_CXX_COMPILER=mpiicpc -DOpenMP_CXX_FLAGS="-qopenmp" -DOpenMP_CXX_LIB_NAMES="libiomp5" -DOpenMP_libiomp5_LIBRARY=/opt/intel/oneapi/compiler/latest/linux/compiler/lib/intel64_lin/libiomp5.so -DCMAKE_INSTALL_PREFIX=$GROMACS_BINARY -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON -DGMX_MPI=on -DGMX_GPU=CUDA -DGMX_CUDA_TARGET_SM="70;75"  -DGMX_OPENMP=ON -DGMX_SIMD=AVX2_256 -DGMX_CYCLE_SUBCOUNTERS=ON -DGMX_OPENMP_MAX_THREADS=32 -DBUILD_SHARED_LIBS=OFF  -DGMX_BUILD_HELP=OFF 
 make -j 8 
 make check
 make install 
